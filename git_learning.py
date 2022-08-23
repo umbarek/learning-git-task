98bad95489fcaa8c2e2c1d2e36b245e61f89bf09
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-08-23 23:16:22 +0200

    Add for loop with parameters

--- git_learning.py
+++ git_learning.py
@@ -3,3 +3,11 @@ print("")
 lista = ("marchew", "seler", "rukola", "chleb", "pączek", "bułki")
 sklep1 = { "warzywniak" : ["marchew","seler","rukola"] }
 sklep2 = { "piekarnia" : ["chleb","pączek","bułki"] }
+print("Lista zakupów:")
+for nazwa in sklep1.keys():
+    for rzecz in sklep1.values():
+        print (f"Idę do {nazwa.capitalize()}, kupuję tu następujące rzeczy:", rzecz)
+("")
+for nazwa in sklep2.keys():
+    for rzecz in sklep2.values():
+        print (f"Idę do {nazwa.capitalize()}, kupuję tu następujące rzeczy:", rzecz)
