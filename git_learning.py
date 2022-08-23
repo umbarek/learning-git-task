2a47417f3c56edae968668b0b5d0274b137cb68f
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-08-23 23:13:12 +0200

    Define dict key and values

---
+++ git_learning.py
@@ -0,0 +1,5 @@
+print("Zadanie_1")
+print("")
+lista = ("marchew", "seler", "rukola", "chleb", "pączek", "bułki")
+sklep1 = { "warzywniak" : ["marchew","seler","rukola"] }
+sklep2 = { "piekarnia" : ["chleb","pączek","bułki"] }
