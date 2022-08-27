be2a835a429be7a06e59558cf5de43493b2aaf44
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-08-27 13:24:59 +0200

    Add new shop name and new product to buy

--- git_learning.py
+++ git_learning.py
@@ -1,8 +1,9 @@
 print("Zadanie_1")
 print("")
-lista = ("marchew", "seler", "rukola", "chleb", "pączek", "bułki")
-sklep1 = { "warzywniak" : ["marchew","seler","rukola"] }
-sklep2 = { "piekarnia" : ["chleb","pączek","bułki"] }
+lista = ("marchew", "seler", "rukola", "chleb", "pączek", "bułki", "sałata", "kapusta", "drozdzówka", "ciasto", "ibuprom")
+sklep1 = { "warzywniak" : ["marchew","seler","rukola", "sałata", "kapusta"]}
+sklep2 = { "piekarnia" : ["chleb","pączek","bułki", "drozdzówka", "ciasto"]}
+sklep3 = {"apteka" : ["ibuprom"]}
 print("Lista zakupów:")
 for nazwa in sklep1.keys():
     for rzecz in sklep1.values():
@@ -11,6 +12,9 @@ for nazwa in sklep1.keys():
 for nazwa in sklep2.keys():
     for rzecz in sklep2.values():
         print (f"Idę do {nazwa.capitalize()}, kupuję tu następujące rzeczy:", rzecz)
-lista = ("marchew", "seler", "rukola", "chleb", "pączek", "bułki")
+("")
+for nazwa in sklep3.keys():
+    for rzecz in sklep3.values():
+        print (f"Idę do {nazwa.capitalize()}, kupuję tu następujące rzeczy:", rzecz)
 print ("W sumie kupię ", len(lista),"produktów")
 print ("To jest koniec zadania")
